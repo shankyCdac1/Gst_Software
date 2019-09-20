afa42ba88dfe212880d1f0db59e0e978c5b38265
shankyCdac1 <shankyCdac@gmail.com>
2019-09-20 12:47:56 +0530

    Add method

--- src/shanky/billing/GstDashboard.java
+++ src/shanky/billing/GstDashboard.java
@@ -156,7 +156,7 @@ public class GstDashboard extends JFrame {
 			total = rs.getString(1);
 		}
 	}
-
+    public String n() {return "";}
 	public void getallvaluesafterCalculation() {
 		int dis = Integer.parseInt(discount.getText());
 		// GROSS Amount
